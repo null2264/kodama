c5e39dacf690d0a83e9d63a0ed410dd1f1787f32
Ahmad Ansori Palembani <palembani@gmail.com>
2026-07-23 16:26:32 +0700

    chore: Bump target and compile sdk

--- buildSrc/src/main/kotlin/AndroidConfig.kt
+++ buildSrc/src/main/kotlin/AndroidConfig.kt
@@ -1,6 +1,6 @@
 object AndroidConfig {
-    const val compileSdk = 35
+    const val compileSdk = 36
     const val minSdk = 23
-    const val targetSdk = 35
+    const val targetSdk = 36
     const val ndk = "28.0.13004108"
 }
