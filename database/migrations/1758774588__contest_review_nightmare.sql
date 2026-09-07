8f8c77cf618f8b5ff016ac78322504838083973e
Ahmad Ansori Palembani <palembani@gmail.com>
2026-09-07 17:39:17 +0700

    fix(database): Allow contestant to see reviews of their bonsai during reviewing stage
    Kinda need it for PPBI flags feature

--- database/migrations/1758774588__contest_review_nightmare.sql
+++ database/migrations/1758774588__contest_review_nightmare.sql
@@ -140,7 +140,7 @@ USING (
       AND b.owner_id = auth.uid()
       AND EXISTS (
         SELECT 1 FROM kodama.contests c
-        WHERE c.id = b.contest_id AND c.state IN ('finished', 'ended')
+        WHERE c.id = b.contest_id AND c.state IN ('reviewing', 'finished', 'ended')
       )
   )
 );
